3de9a3cca1c0d46f6929e32a48af6b197f0dcf93
smart-life-tech <christlightworld@gmail.com>
2023-12-13 22:00:37 +0100

    new file:   normalRecording.py

---
+++ i2smemsTest.py
@@ -0,0 +1,18 @@
+import sounddevice as sd
+import numpy as np
+import scipy.io.wavfile as wav
+
+# Set the recording parameters
+duration = 5  # seconds
+sample_rate = 44100  # adjust based on your microphone specifications
+
+# Record audio
+print("Recording...")
+audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
+sd.wait()
+print("Recording done.")
+
+# Save the recorded audio to a WAV file
+wav.write('test.wav', sample_rate, audio_data)
+
+print("Audio saved to test.wav.")
---
+++ normalRecording.py
@@ -0,0 +1,48 @@
+import librosa
+import numpy as np
+import pyaudio
+
+def record_audio(seconds=5, sample_rate=44100):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=sample_rate,
+                    input=True,
+                    frames_per_buffer=1024)
+
+    print("Recording...")
+
+    frames = []
+    for _ in range(int(sample_rate / 1024 * seconds)):
+        data = stream.read(1024)
+        frames.append(np.frombuffer(data, dtype=np.float32))
+
+    print("Recording done.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    return np.concatenate(frames)
+
+def compare_audio(live_audio, reference_audio):
+    # Use librosa to compute cross-correlation
+    cross_corr = np.correlate(live_audio, reference_audio, mode='full')
+
+    # Find the index of the maximum correlation
+    max_corr_index = np.argmax(cross_corr)
+
+    return max_corr_index
+
+# Replace 'reference_audio.wav' with the path to your reference audio file
+reference_file = 'reference_audio.wav'
+
+# Load the reference audio file
+reference_audio, _ = librosa.load(reference_file, sr=None, mono=True)
+
+# Record live audio (adjust the recording time as needed)
+live_audio = record_audio(seconds=5)
+
+# Compare live audio with reference audio
+correlation_index = compare_audio(live_audio, reference_audio)
+
+print("Maximum correlation index:", correlation_index)
